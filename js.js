aefcebcb25a72528185c9bf4f7b16fc5dc05be48
danilo-89 <danilo89abcd@gmail.com>
2022-02-13 21:39:31 +0100

    general fixes

--- js.js
+++ js.js
@@ -2,6 +2,10 @@ const nav = document.getElementById('nav');
 const navBtn = document.getElementById('navBtn');
 const closeBtn = document.getElementById('closeBtn');
 const navHolder = document.getElementById('navHolder');
+const calcForm = document.getElementById('calcForm');
+const humanAgeInput = document.getElementById('humanAge');
+const catAgeInput = calcForm.elements['catAge'];
+const errorSpan = document.getElementById('errorSpan');
 
 navBtn.addEventListener('click', () => {
 	nav.classList.add('expanded');
@@ -17,10 +21,33 @@ navHolder.addEventListener('click', function (event) {
 	}
 });
 
+calcForm.addEventListener('submit', function (event) {
+	event.preventDefault();
+	calcCatAge();
+	catAgeInput.blur();
+});
+
+catAgeInput.addEventListener('input', function (event) {
+	calcCatAge();
+});
+
 function calcCatAge() {
-	var catAge = document.getElementsByName('catAge')[0].value;
-	var humanYears = 4 * (catAge - 2) + 24;
-	console.log(catAge);
-	console.log(humanYears);
-	document.getElementById('humanAge').innerHTML = humanYears;
+	errorSpan.innerText = '';
+	let catAge = +catAgeInput.value;
+	let humanYears;
+
+	if (catAge >= 0.1 && catAge <= 1) {
+		humanYears = 15 * +catAge.toFixed(1);
+	} else if (catAge > 1 && catAge <= 2) {
+		humanYears = 15 + 9 * (+catAge.toFixed(1) - 1);
+	} else if (catAge > 2 && catAge <= 45) {
+		humanYears = 4 * +catAge.toFixed(1) + 16;
+	} else {
+		humanAgeInput.innerText = '?';
+		errorSpan.innerText = 'Please enter valid age of a cat';
+		return;
+	}
+
+	humanAgeInput.innerText = +humanYears.toFixed(1);
+	return;
 }
