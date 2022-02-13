57fe0b7efa0671361d40ab7b5997ed5bc5feb20f
danilo-89 <danilo89abcd@gmail.com>
2022-02-13 02:51:59 +0100

    css and html changes

--- js.js
+++ js.js
@@ -1,16 +1,21 @@
-console.log("test");
+const nav = document.getElementById('nav');
+const navBtn = document.getElementById('navBtn');
+
+navBtn.addEventListener('click', () => {
+	nav.classList.add('expanded');
+});
 
 function myFunction() {
-  return this;
+	return this;
 }
 
 console.log(myFunction());
-console.log("fgdfg");
+console.log('fgdfg');
 
 function calcCatAge() {
-  var catAge = document.getElementsByName("catAge")[0].value;
-  var humanYears = 4*(catAge - 2) + 24;
-  console.log(catAge);
-  console.log(humanYears);
-  document.getElementById("humanAge").innerHTML = humanYears;
+	var catAge = document.getElementsByName('catAge')[0].value;
+	var humanYears = 4 * (catAge - 2) + 24;
+	console.log(catAge);
+	console.log(humanYears);
+	document.getElementById('humanAge').innerHTML = humanYears;
 }
